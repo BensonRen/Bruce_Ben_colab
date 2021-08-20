86fc17e085c412bc82036edf046302d19da2f0c7
Ben Ren <rensimiao.ben@gmail.com>
2021-08-20 17:21:44 -0400

    first com

---
+++ data_reader.py
@@ -0,0 +1,173 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset
+from sklearn.model_selection import train_test_split
+import torch
+
+
+def get_data_into_loaders(data_x, data_y, batch_size, DataSetClass, rand_seed=1, test_ratio=0.05):
+    """
+    Helper function that takes structured data_x and data_y into dataloaders
+    :param data_x: the structured x data
+    :param data_y: the structured y data
+    :param rand_seed: the random seed
+    :param test_ratio: The testing ratio
+    :return: train_loader, test_loader: The pytorch data loader file
+    """
+    # Normalize the input
+    # x_train, x_test, y_train, y_test = train_test_split(data_x, data_y, test_size=test_ratio,
+    #                                                    random_state=rand_seed)
+    
+    # new shuffle scheme to account for the bruce dataset
+    train_test_bound = 1237
+    x_train, y_train = data_x[:train_test_bound, :], data_y[:train_test_bound]
+    x_test, y_test = data_x[train_test_bound:, :], data_y[train_test_bound:]
+    
+    # Get the length of the training data
+    train_len, test_len = len(x_train), len(x_test)
+    
+    # Shuffle them
+    train_shuffle_index = np.random.permutation(train_len)
+    test_shuffle_index = np.random.permutation(test_len)
+
+    x_train = x_train[train_shuffle_index]
+    y_train = y_train[train_shuffle_index]
+    x_test = x_test[test_shuffle_index]
+    y_test = y_test[test_shuffle_index]
+    
+    print('total number of training sample is {}, the dimension of the feature is {}'.format(len(x_train),
+                                                                                             len(x_train[0])))
+    print('total number of test sample is {}'.format(len(y_test)))
+
+    # Construct the dataset using a outside class
+    train_data = DataSetClass(x_train, y_train)
+    test_data = DataSetClass(x_test, y_test)
+
+    # Construct train_loader and test_loader
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size)
+
+    return train_loader, test_loader
+
+
+def normalize_np(x):
+    """
+    Normalize the x into [-1, 1] range in each dimension [:, i]
+    :param x: np array to be normalized
+    :return: normalized np array
+    """
+    for i in range(len(x[0])):
+        x_max = np.max(x[:, i])
+        x_min = np.min(x[:, i])
+        x_range = (x_max - x_min) / 2.
+        x_avg = (x_max + x_min) / 2.
+        x[:, i] = (x[:, i] - x_avg) / x_range
+        print("In normalize_np, row ", str(i), " your max is:", np.max(x[:, i]))
+        print("In normalize_np, row ", str(i), " your min is:", np.min(x[:, i]))
+        assert np.max(x[:, i]) - 1 < 0.0001, 'your normalization is wrong'
+        assert np.min(x[:, i]) + 1 < 0.0001, 'your normalization is wrong'
+    return x
+
+
+def read_data_bruce(flags, eval_data_all=False):
+    # Read the data
+    data_dir = '/home/sr365/Bruce/cvdata'
+    print("data_dir = ", data_dir)
+    data = np.load(os.path.join(data_dir, str(flags.comp_ind) + '.npy'))
+    data = data.astype(np.float32)
+    data_x = data[:, 1:]
+    data_y = data[:, 0]
+    if eval_data_all:
+        return get_data_into_loaders(data_x, data_y, flags.batch_size, 
+                SimulatedDataSet_xd_to_1d_class, test_ratio=0.98)
+    return get_data_into_loaders(data_x, data_y, flags.batch_size, SimulatedDataSet_xd_to_1d_class,
+                                 test_ratio=flags.test_ratio)
+
+
+def read_data(flags, eval_data_all=False):
+    """
+    The data reader allocator function
+    The input is categorized into couple of different possibilities
+    :param flags: The input flag of the input data set
+    :param eval_data_all: The switch to turn on if you want to put all data in evaluation data
+    :return:
+    """
+    print("In read_data, flags.data_set =", flags.data_set)
+    if 'bruce' in flags.data_set or 'Bruce' in flags.data_set:
+        train_loader, test_loader = read_data_bruce(flags, eval_data_all=eval_data_all)
+    else:
+        sys.exit("Your flags.data_set entry is not correct, check again!")
+    return train_loader, test_loader
+
+
+class MetaMaterialDataSet(Dataset):
+    """ The Meta Material Dataset Class """
+
+    def __init__(self, ftr, lbl, bool_train):
+        """
+        Instantiate the Dataset Object
+        :param ftr: the features which is always the Geometry !!
+        :param lbl: the labels, which is always the Spectra !!
+        :param bool_train:
+        """
+        self.ftr = ftr
+        self.lbl = lbl
+        self.bool_train = bool_train
+        self.len = len(ftr)
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, ind):
+        return self.ftr[ind, :], self.lbl[ind, :]
+
+
+class SimulatedDataSet_class_1d_to_1d(Dataset):
+    """ The simulated Dataset Class for classification purposes"""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.len = len(x)
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, ind):
+        return self.x[ind], self.y[ind]
+
+
+class SimulatedDataSet_xd_to_1d_class(Dataset):
+    """ The simulated Dataset Class for classification purposes"""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.len = len(x)
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, ind):
+        return self.x[ind, :], self.y[ind]
+
+
+class SimulatedDataSet_regress(Dataset):
+    """ The simulated Dataset Class for regression purposes"""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.len = len(x)
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, ind):
+        return self.x[ind, :], self.y[ind, :]
---
+++ train.py
@@ -0,0 +1,100 @@
+"""
+This file serves as a training interface for training the network
+"""
+# Built in
+import glob
+import os
+import pandas as pd
+import numpy as np
+import sys
+# Torch
+
+# Own
+import flag_reader
+import data_reader
+from class_wrapper import Network
+from model_maker import Forward
+from helper_functions import put_param_into_folder, write_flags_and_BVE
+
+
+def training_from_flag(flags):
+    """
+    Training interface. 1. Read data 2. initialize network 3. train network 4. record flags
+    :param flag: The training flags read from command line or parameter.py
+    :return: None
+    """
+    
+    # Get the data
+    train_loader, test_loader = data_reader.read_data(flags)
+
+    print("Making network now")
+
+    # Make Network
+    ntwk = Network(Forward, flags, train_loader, test_loader)
+    total_param = sum(p.numel() for p in ntwk.model.parameters() if p.requires_grad)
+    print("Total learning parameter is: %d"%total_param)
+    
+    # Training process
+    print("Start training now...")
+    ntwk.train()
+
+    # Do the house keeping, write the parameters and put into folder, also use pickle to save the flags obejct
+    write_flags_and_BVE(flags, ntwk)
+    # put_param_into_folder(ntwk.ckpt_dir)
+
+
+def get_list_comp_ind(gpu):
+    """
+    The function to get the list of comp index by gpu number from 0-3
+    if GPU==-1, return the list of all the models
+    """
+    if gpu not in [0, 1, 2, 3, -1]:
+        print('Your gpu index is not correct, check again')
+        quit()
+    data_dir = '/home/sr365/Bruce/cvdata'
+    ind_list = []
+    for file in os.listdir(data_dir):
+        #print(file)
+        # Check if this is a comp file
+        if not file.endswith('.npy') or (not file[:-4].isdigit()):
+            print('This file is {}, does not satisfy requirement, continue'.format(file))
+            continue
+        ind = int(file[:-4])
+        #print('current comp ind is {}'.format(ind))
+        ind_list.append(ind)
+    #print(ind_list)
+    length = len(ind_list)
+    print(length)
+    # If GPU == -1, return all list values
+    if gpu == -1:
+        return ind_list
+    gpu_specific_list = ind_list[gpu*int(length / 4):(gpu+1)*int(length / 4)]
+    print(len(gpu_specific_list))
+    return gpu_specific_list
+  
+
+
+def train_all_models(gpu):
+    """
+    The aggregate training
+    """
+    comp_ind_list = get_list_comp_ind(gpu)
+    for hidden_layer_num in [7]:
+    #for hidden_layer_num in [1, 3, 5, 7]:
+        for neurons in [20, 50, 100, 200, 500]:
+            for comp_ind in comp_ind_list:
+                flags = flag_reader.read_flag()
+                flags.comp_ind = comp_ind
+                flags.linear = [flags.dim_x] + [neurons for i in range(hidden_layer_num)] + [flags.dim_y]
+                flags.model_name = flags.data_set + '_ind_' + str(flags.comp_ind) + '_complexity_{}x{}_lr_{}_decay_{}_reg_{}_bs_{}'.format(flags.linear[1], len(flags.linear) - 2, flags.lr, flags.lr_decay_rate, flags.reg_scale, flags.batch_size)
+                print(flags.model_name)
+                training_from_flag(flags)
+
+if __name__ == '__main__':
+    # Read the parameters to be set
+    flags = flag_reader.read_flag()
+    # Call the train from flag function
+    training_from_flag(flags)
+    #train_all_models(-1)
+    #for i in range(4):
+    #    get_list_comp_ind(i)
